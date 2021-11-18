661391dfe5929b3d32b7239a5aac2c3cc24b5a2f
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2021-11-18 02:00:46 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+
 echo "Input amount of nodes to run: "
 read x
-
-touch log1 log2
 
 sudo docker build -t mxnode .
 
 gateway=$(sudo docker network inspect bridge | grep Gateway | awk '{print substr($2,2,length($2)-3)}')
 ip=$(sudo docker network inspect bridge | grep Gateway | awk '{print substr($2,length($2)-1,1)}')
+((ip+=3))
 
-mkdir -p ${PWD}/log
+mkdir -p log
+rm ${PWD}/log/*.txt
 
-sudo docker run --rm -v ${PWD}/log:/go/src/app/log -e NODE_NAME=node1 -e ADVERTISE_ADDRESS=${gateway}$((ip+1)) -p 8080:8080 mxnode &
-for i in {0..${x}}
+touch ${PWD}/log/node1.txt
+sudo docker run --rm -v ${PWD}/log/node1.txt:/go/src/app/log/log.txt -e NODE_NAME=node1 -e ADVERTISE_ADDRESS=${gateway}$((ip+1)) -p 8080:8080 mxnode & > /dev/null
+for i in $(eval echo {0..$((x-2))})
 do
-  sudo docker run --rm -v ${PWD}/log:/go/src/app/log -e NODE_NAME=node${i+2} -e ADVERTISE_ADDRESS=${gateway}$((ip+i+1)) -e CLUSTER_ADDRESS=${gateway}$((ip+i)) -p $((8080+i)):8080 mxnode &
+  touch ${PWD}/log/node${i+2}.txt
+  sudo docker run --rm -v ${PWD}/log/node${i+2}.txt:/go/src/app/log/log.txt -e NODE_NAME=node${i+2} -e ADVERTISE_ADDRESS=${gateway}$((ip+i+2)) -e CLUSTER_ADDRESS=${gateway}$((ip+i+1)) -p $((8080+i+1)):8080 mxnode & > /dev/null
 done
